063800a51eea2eecf265ff1c53809d5e85aa8b2a
MR.ARIS <70155682+ariszywlafa1@users.noreply.github.com>
2020-10-03 11:00:24 +0700

    Create wa.php

---
+++ wa.php
@@ -0,0 +1,159 @@
+<?php
+// Recode Sepuas nya°_°
+         // ngapain tong,jangan recode
+         // NGAPAIN NANO AMJINF
+         // Ini Cuman Script Prank Ya Amjinc:v
+echo "Berapa Umurmu->>";
+ switch(trim(fgets(STDIN)))
+       {}
+echo "Pilih Yang Mana Bro->>";
+ switch(trim(fgets(STDIN)))
+       {}
+system('clear');
+   echo " MEMBUKA [>    ]";
+  system('sleep 0.5');
+system('clear');
+   echo " MEMBUKA [>>   ]";
+  system('sleep 0.5');
+system('clear');
+   echo " MEMBUKA [>>>  ]";
+  system('sleep 0.5');
+system('clear');
+   echo " MEMBUKA [>>>> ]";
+system('sleep 0.5');
+system('clear');
+   echo " MEMBUKA [>>>>>]";
+  system('sleep 0.5');
+system('clear');
+system('neofetch');
+
+    $green  = "\e[92m";
+    $red    = "\e[91m";
+    $yellow = "\e[93m";
+    $blue   = "\e[36m";
+    $number = "number";
+system('sleep 0.5');
+echo $blue."
+█████████ 
+█▄█████▄█   
+█ ▼▼▼▼▼ █
+█       █   AUTHOR :  ARIS  \n";
+echo $green."█ ▲▲▲▲▲ █ 
+█████████    TEAM   :  MAris
+ ██___██\n";
+system('sleep 0.4');
+
+echo $red."
+1.Tembak Password Fb
+2.Tembak Password Wa
+3.Download Full Script By MAris\n";
+echo $yellow."
+Pilih Yang Mana Bro->>";
+ switch(trim(fgets(STDIN)))
+       {
+case 3:
+echo "Anda Akan Mendownload Script\n";
+echo "Membuka Browser\n";
+system('sleep 1');
+system('xdg-open https://sfile.mobi/aBO53PfC5ql');
+exit();
+case 1:
+system('clear');
+system('neofetch');
+echo "Masukan ID / Link Akun Target:";
+switch(trim(fgets(STDIN)))
+        {}
+echo "Siapa Nama Akun Target?:";
+switch(trim(fgets(STDIN)))
+        {}
+exit();
+case 2:
+system('clear');
+system('neofetch');
+echo "Masukan Nomor Korban:";
+switch(trim(fgets(STDIN)))
+        {}
+echo "Apakah Anda Akan Hack? (Y/N):";
+switch(trim(fgets(STDIN)))
+        {
+case y:
+system('clear');
+system('sleep 1');
+echo "Tunggu Sedang Mengkonfirmasi\n";
+system('sleep 1');
+system('clear');
+echo "Tunggu Sedang Mengkonfirmasi.\n";
+ system('sleep 1');
+system('clear');
+echo "Tunggu Sedang Mengkonfirmasi..\n";
+system('sleep 1');
+system('clear');
+echo "Tunggu Sedang Mengkonfirmasi...\n";
+system('sleep 1');
+system('clear');
+   echo " SEDANG MENUNGGU KODE [>    ]";
+  system('sleep 1');
+system('clear');
+   echo " SEDANG MENUNGGU KODE [>>   ]";
+  system('sleep 1');
+system('clear');
+   echo " SEDANG MENUNGGU KODE [>>>  ]";
+  system('sleep 1');
+system('clear');
+   echo " SEDANG MENUNGGU KODE [>>>> ]";
+system('sleep 1');
+system('clear');
+   echo " SEDANG MENUNGGU KODE [>>>>>]";
+system('clear');
+system('sleep 1');
+echo " Kode Whatsappnya Adalah 618169\n";
+system('sleep 10000');
+exit();
+case Y:
+system('clear');
+system('sleep 1');
+echo "Tunggu Sedang Mengkonfirmasi\n";
+system('sleep 1');
+system('clear');
+echo "Tunggu Sedang Mengkonfirmasi.\n";
+ system('sleep 1');
+system('clear');
+echo "Tunggu Sedang Mengkonfirmasi..\n";
+system('sleep 1');
+system('clear');
+echo "Tunggu Sedang Mengkonfirmasi...\n";
+system('sleep 1');
+system('clear');
+   echo " SEDANG MENUNGGU KODE [>    ]";
+  system('sleep 1');
+system('clear');
+   echo " SEDANG MENUNGGU KODE [>>   ]";
+  system('sleep 1');
+system('clear');
+   echo " SEDANG MENUNGGU KODE [>>>  ]";
+  system('sleep 1');
+system('clear');
+   echo " SEDANG MENUNGGU KODE [>>>> ]";
+system('sleep 1');
+system('clear');
+   echo " SEDANG MENUNGGU KODE [>>>>>]";
+system('clear');
+system('sleep 1');
+echo " Kode Whatsappnya Adalah 618169\n";
+system('sleep 10000');
+exit();
+            case n:
+system('clear');
+echo "ANDA KELUAR DARI SCRIPT\n";
+           system('sleep 3');
+           exit();
+case N:
+system('clear');
+echo "ANDA KELUAR DARI SCRIPT\n";
+           system('sleep 3');
+           exit();
+        }
+}
+    // Close standard in..
+    fclose(STDIN);
+?>
