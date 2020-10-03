05e816b7b3a2cd4ce90b3764d8af5d96e391acaa
MR.ARIS <70155682+ariszywlafa1@users.noreply.github.com>
2020-10-03 20:55:46 +0700

    Update aris.php

--- aris.php
+++ aris.php
@@ -1,2 +1,21 @@
 <?php
-echo "Masukan
+echo "
+1.Masukan Kode
+2.Download Kode\n";
+echo ."
+Pilih==>>";
+switch(trim(fgets(STDIN)))
+       {
+case 2:
+echo "Hai";
+exit();
+case 1:
+system('sleep 1');
+echo "Kodenya Apa:";
+switch(trim(fgets(STDIN)))
+       {
+case MAris:
+system('sleep 1');
+echo "Kode Benar!!
+system('sleep 1');
+exit();
