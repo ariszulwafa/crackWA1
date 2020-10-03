870108fde96ce76e313e75b0a082f08e0ac339ed
MR.ARIS <70155682+ariszywlafa1@users.noreply.github.com>
2020-10-03 21:07:50 +0700

    Update aris.php

--- aris.php
+++ aris.php
@@ -3,13 +3,13 @@ system('clear');
 echo "
 1.Masukan Kode
 2.Download Kode\n";
-echo ."Pilih==>>";
+echo "Pilih==>>";
 switch(trim(fgets(STDIN)))
        {
 case 2:
 echo "Download Filenya Dan Ekstrak,Lalu Lihat File txt\n";
 system('sleep 0.5');
-echo "Membuka Browser\n";;
+echo "Membuka Browser\n";
 system('sleep 1');
 system('xdg-open https://sfile.mobi/38DpJ1eFGw7');
 system('sleep 1');
@@ -17,7 +17,7 @@ system('php aris.php');
 exit();
 case 1:
 system('sleep 1');
-echo "Kodenya Apa:";
+echo "Masukkan Kode:";
 switch(trim(fgets(STDIN)))
        {
 case MAris:
